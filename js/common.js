7a576c94a41a7578860b2815e7e68aad5edcf34d
Gulin Sergey <sg@webberry.ru>
2017-09-08 15:14:51 +0300

    всё кроме favicon

--- js/common.js
+++ js/common.js
@@ -1,5 +1,12 @@
 $(function() {
 
+	var slider = new IdealImageSlider.Slider({
+		selector: '#slider',
+		height: 400, // Required but can be set by CSS
+		interval: 5000,
+		transitionDuration: 800
+		});
+	slider.start();
 	// Custom JS
 
 });
